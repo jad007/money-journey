ca955e2a67a6b7c074c63e8616fd47072610de88
Jared Duncan <jduncan@iqnavigator.com>
2016-03-25 14:06:04 -0600

    initializing spring boot

---
+++ src/main/groovy/com/jad007/MoneyJourneyApplication.groovy
@@ -0,0 +1,12 @@
+package com.jad007
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MoneyJourneyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run MoneyJourneyApplication, args
+	}
+}
